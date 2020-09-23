743f006092fdccedf8b6e96064024e884666b5c3
kaprinyakdev@gmail.com <kaprinyakdev@gmail.com>
2020-09-23 09:41:47 +0200

    test

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart'
+
+void main(){
+    runApp(Asd());
+}
+
+class Asd extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Text('asd')
+      );
+  }
+}
