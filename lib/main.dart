6fb9f1541c9664cba746ca700e1069878ab0acdc
Kaprinyák Péter <kaprinyak.peter@evolution-consulting.hu>
2020-09-23 14:45:32 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestionFunction(){
+      print('Answer chosen!');
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    
+    var questions = ['Age?', 'Place of birth?'];
+
     return MaterialApp(
-      home: Text('teszt'));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text('question'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestionFunction,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestionFunction,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestionFunction,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
